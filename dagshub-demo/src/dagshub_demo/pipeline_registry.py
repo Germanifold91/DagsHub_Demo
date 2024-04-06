25d8bb7c319071142ff74b4cdccd57d4bdcf1edc
Germanifold91 <germanifold@outlook.com>
2024-04-06 15:25:45 -0500

    Update of pipeline registry

--- dagshub-demo/src/dagshub_demo/pipeline_registry.py
+++ dagshub-demo/src/dagshub_demo/pipeline_registry.py
@@ -12,7 +12,10 @@ from dagshub_demo import __version__ as PROJECT_VERSION
 from .pipelines.data_engineering.pipeline import (
     data_processing as data_enginering_pipeline,
 )
-from .pipelines.data_modeling.pipeline import ml_pipeline as model_training_pipeline
+from .pipelines.data_modeling.pipeline import (
+    ml_pipeline as model_training_pipeline,
+)
+from .pipelines.user_app.pipeline import create_user_app_pipeline
 
 
 def register_pipelines() -> Dict[str, Pipeline]:
@@ -22,7 +25,6 @@ def register_pipelines() -> Dict[str, Pipeline]:
         A mapping from pipeline names to ``Pipeline`` objects.
     """
     data_processing_pipeline = data_enginering_pipeline()
-
     ml_pipeline = model_training_pipeline()
     data_transforming_pipeline = ml_pipeline.only_nodes_with_tags("data_transformation")
     inference_pipeline = ml_pipeline.only_nodes_with_tags("inference")
@@ -41,12 +43,16 @@ def register_pipelines() -> Dict[str, Pipeline]:
             signature="auto",
         ),
     )
+    user_app_pipeline = create_user_app_pipeline()
     return {
         "data_processing": data_processing_pipeline,
         "data_transforming": data_transforming_pipeline,
         "training": training_pipeline_ml,
+        "inference": inference_pipeline,
+        "user_app": user_app_pipeline,
         "__default__": data_processing_pipeline
         + data_transforming_pipeline
         + training_pipeline_ml
-        + inference_pipeline,
+        + inference_pipeline
+        + user_app_pipeline,
     }
